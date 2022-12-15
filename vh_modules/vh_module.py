24c0b03fc5b363b5daa42cec837a05a5b9125b90
AndriiGrytsenkoVH <andrew.grit.vh@gmail.com>
2022-12-14 19:31:16 -0600

    updated my module

--- vh_modules/vh_module.py
+++ vh_modules/vh_module.py
@@ -1,3 +1,13 @@
+from time import time
+
+def time_this_func(func, iterations, *args):
+    start = time()
+    for _ in range(iterations):
+        func(*args)
+    end = time()
+    print(f"{func}")
+    print(end - start)
+
 def print_init(string_of_things):
     """
     Prints out attributes for __init__ of a class using it's arguments
